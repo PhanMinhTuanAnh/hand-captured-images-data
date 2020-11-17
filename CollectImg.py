96589bbe104cd34e5a0930cf1fd7e66713948510
Phan Minh Tuan Anh <anhphanaabb@gmail.com>
2020-11-18 01:25:18 +0700

    first-commit

---
+++ CollectImg.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+import time
+import uuid
+
+IMAGES_PATH = "collect-images43"
+labels = ['write1', 'write2', 'write3', 'wait', 'stop', 'back', 'check']
+number_imgs = 20
+i = 0
+for label in labels:
+    cap = cv2.VideoCapture(0)
+    codec = 0x47504A4D  # MJPG
+    cap.set(cv2.CAP_PROP_FPS, 30.0)
+    cap.set(cv2.CAP_PROP_FOURCC, codec)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    print("Collecting images for {}".format(label))
+    time.sleep(5)
+    for imgnum in range(number_imgs):
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 1)
+        imagename = os.path.join(IMAGES_PATH + "/" + label + "." + "{}.jpg".format(str(61+imgnum)))
+        i = i + 1
+        print(imagename)
+        cv2.imwrite(imagename, frame)
+        cv2.imshow('frame', frame)
+        time.sleep(2)
+
+        if cv2.waitKey(1) and 0xFF == ord('q'):
+            break
+
+    cap.release()
